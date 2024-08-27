b6170c27749e495a85a93e20534f154b5de7bab7
John Bogart <johnjackbogart@gmail.com>
2024-08-27 11:30:19 -0400

    messing around with gradients, added my icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-tr from-[#000000] from-75% via-teal-500 to-[#ffffff] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Create <span className="text-[hsl(0,0%,0%)]">T3</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
@@ -40,12 +35,8 @@ export default async function Home() {
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
+            <p className="text-2xl text-white">hello</p>
           </div>
-
-          <LatestPost />
         </div>
       </main>
     </HydrateClient>
