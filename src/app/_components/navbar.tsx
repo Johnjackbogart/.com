6b171f7b116e26424d45f3047207b8ec67edff5c
John Bogart <johnjackbogart@gmail.com>
2024-09-17 18:37:35 -0400

    fixed path aliases, extra fixed, and moved _components

---
+++ src/app/_components/navbar.tsx
@@ -0,0 +1,69 @@
+"use client";
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "#/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "#/dropdown-menu";
+import JackBogart from "^/JackBogart";
+import Github from "^/GitHub";
+
+export const Nav = () => {
+  const { setTheme } = useTheme();
+  return (
+    <div
+      className={
+        "w-m z-50 flex h-14 items-center overflow-visible border-b border-border bg-card px-4 py-2"
+      }
+    >
+      <div className={"mt-auto"}>
+        <JackBogart />
+      </div>
+      <div className={"ml-auto flex items-center gap-1"}>
+        <Button
+          onClick={() => {
+            window.open(
+              //https://stackoverflow.com/questions/50822310/how-to-import-package-json-in-typescript
+              //I don't think this will break anything on vercel??
+              process.env.npm_package_homepage,
+              "_blank",
+              "noopener noreferrer",
+            );
+          }}
+          variant={"ghost"}
+          className={"ml-auto flex items-center gap-1.5"}
+        >
+          <span>
+            <Github className={"size-4"} />
+          </span>
+        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/app/_components/three/playground.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Box, Torus } from "@react-three/drei";
+import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
+
+export default function PlayGround() {
+  return (
+    <Canvas>
+      <Suspense>
+        <Physics gravity={[0, 0, 0]} debug>
+          <RigidBody
+            colliders={"hull"}
+            restitution={2}
+            rotation={[0, 0.3, 2]}
+            angularVelocity={[0, 1, 0]}
+          >
+            <Torus />
+          </RigidBody>
+        </Physics>
+      </Suspense>
+    </Canvas>
+  );
+}
