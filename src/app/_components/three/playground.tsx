96e2f2425776ac358a6af48ed722210a0c72697f
John Bogart <johnjackbogart@gmail.com>
2024-09-17 19:26:30 -0400

    testing dev deployment

--- src/app/_components/three/playground.tsx
+++ src/app/_components/three/playground.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Box, Torus } from "@react-three/drei";
+import { Box, TorusKnot } from "@react-three/drei";
 import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
 
 export default function PlayGround() {
@@ -15,7 +15,7 @@ export default function PlayGround() {
             rotation={[0, 0.3, 2]}
             angularVelocity={[0, 1, 0]}
           >
-            <Torus />
+            <TorusKnot args={[]} />
           </RigidBody>
         </Physics>
       </Suspense>
