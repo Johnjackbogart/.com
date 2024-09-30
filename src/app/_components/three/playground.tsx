c0c2b92d6f8f2b426d5422574560fea40a3fae52
John Bogart <johnjackbogart@gmail.com>
2024-09-30 15:27:42 -0400

    current state

--- src/app/_components/three/playground.tsx
+++ src/app/_components/three/playground.tsx
@@ -2,22 +2,26 @@
 import * as THREE from "three";
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import { TorusKnot } from "@react-three/drei";
 import { Physics, RigidBody } from "@react-three/rapier";
 
 export default function PlayGround() {
   const tk = useRef<THREE.Mesh>(null);
 
-  let p = 1;
-  let q = 1;
+  let p = 10;
+  let q = 10;
 
   useFrame((state) => {
-    p = Math.ceil(Math.abs(100 * Math.sin(0.1 * state.clock.getElapsedTime())));
-    q = Math.ceil(Math.abs(200 * Math.sin(0.1 * state.clock.getElapsedTime())));
+    p = Math.ceil(
+      Math.abs(100 * Math.sin(0.02 * state.clock.getElapsedTime())),
+    );
+
+    q = Math.ceil(
+      Math.abs(200 * Math.cos(0.01 * state.clock.getElapsedTime())),
+    );
 
     if (tk.current) {
-      tk.current.rotation.y = 4 * state.clock.getElapsedTime();
-      tk.current.rotation.x = (8 * state.clock.getElapsedTime()) / 2;
+      tk.current.rotation.y = 1 * state.clock.getElapsedTime();
+      tk.current.rotation.x = (1 * state.clock.getElapsedTime()) / 2;
 
       //this is the only way...
       //https://stackoverflow.com/questions/40933735/three-js-cube-geometry-how-to-update-parameters
@@ -25,8 +29,8 @@ export default function PlayGround() {
       tk.current.geometry = new THREE.TorusKnotGeometry(
         2,
         0.01,
-        1000,
-        100,
+        10000,
+        10,
         p,
         q,
       );
@@ -37,7 +41,7 @@ export default function PlayGround() {
     <Physics gravity={[0, 0, 0]}>
       <RigidBody colliders={"hull"} restitution={2}>
         <mesh ref={tk}>
-          <torusKnotGeometry args={[2, 0.01, 1000, 100, p, q]} />
+          <torusKnotGeometry args={[2, 0.001, 1000, 1000, p, q]} />
           <meshStandardMaterial color="black" metalness={0.6} roughness={0.4} />
         </mesh>
       </RigidBody>
