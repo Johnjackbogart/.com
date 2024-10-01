5059205a0bfadd7911566db3856003d80c0ce36e
John Bogart <johnjackbogart@gmail.com>
2024-10-01 11:37:02 -0400

    look at me, adding a model of myself to scene, some translucent stuff, woohooo

--- src/app/_components/three/playground.tsx
+++ src/app/_components/three/playground.tsx
@@ -3,7 +3,13 @@ import * as THREE from "three";
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { Physics, RigidBody } from "@react-three/rapier";
-import { OrbitControls } from "@react-three/drei";
+import {
+  Text,
+  OrbitControls,
+  Line,
+  DragControls,
+  MeshTransmissionMaterial,
+} from "@react-three/drei";
 
 import { Me } from "./Me";
 
@@ -15,23 +21,23 @@ export default function PlayGround() {
 
   useFrame((state) => {
     p = Math.ceil(
-      Math.abs(100 * Math.sin(0.02 * state.clock.getElapsedTime())),
+      Math.abs(100 * Math.sin(0.0002 * state.clock.getElapsedTime())),
     );
 
     q = Math.ceil(
-      Math.abs(200 * Math.cos(0.01 * state.clock.getElapsedTime())),
+      Math.abs(200 * Math.sin(0.0003 * state.clock.getElapsedTime())),
     );
 
     if (tk.current) {
       tk.current.rotation.y = 1 * state.clock.getElapsedTime();
-      tk.current.rotation.x = (1 * state.clock.getElapsedTime()) / 2;
+      //tk.current.rotation.x = (1 * state.clock.getElapsedTime()) / 2;
 
       //this is the only way...
       //https://stackoverflow.com/questions/40933735/three-js-cube-geometry-how-to-update-parameters
       tk.current.geometry.dispose();
       tk.current.geometry = new THREE.TorusKnotGeometry(
         2,
-        0.01,
+        0.21,
         10000,
         10,
         p,
@@ -42,13 +48,28 @@ export default function PlayGround() {
 
   return (
     <Physics gravity={[0, 0, 0]}>
-      <ambientLight intensity={5} />
+      <Text color="green">yooo</Text>
+      <Line
+        points={[
+          [0, 0, 0],
+          [1, 1, 1],
+        ]}
+      />
+      <ambientLight intensity={1} />
+
+      <pointLight position={[0, 0, 0]} />
       <OrbitControls />
-      <Me />
+      <DragControls>
+        <Me />
+      </DragControls>
       <RigidBody colliders={"hull"} restitution={2}>
         <mesh ref={tk}>
           <torusKnotGeometry args={[2, 0.001, 1000, 1000, p, q]} />
-          <meshStandardMaterial color="black" metalness={0.6} roughness={0.4} />
+          <MeshTransmissionMaterial
+            thickness={2}
+            backside
+            backsideThickness={1}
+          />
         </mesh>
       </RigidBody>
     </Physics>
